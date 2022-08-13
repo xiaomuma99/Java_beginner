da423e33cc52433a4b7ffe8d3c35f65e13923f4f
xiaomuma99 <60707406+xiaomuma99@users.noreply.github.com>
2022-08-13 15:02:41 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+1. All code in Java must be part of a Class
+2. We delimit the beginning and end of segments of code using { and }.
+3. ALl statements in Java must end in a semi-colon.
+4. For code to run we need public static void main(String[] args)
+
+ */
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
